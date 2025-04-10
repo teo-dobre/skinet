359ccfd6d03f3255fdc8abfb27e988ae856b65a4
Teodor Dobre <tdobre88@gmail.com>
2025-04-10 17:17:47 +0300

    End of section 4

---
+++ Core/Specifications/BrandListSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications;
+public class BrandListSpecification : BaseSpecification<Product, string>
+{
+    public BrandListSpecification()
+    {
+        AddSelect(x => x.Brand);
+        ApplyDistinct();
+    }
+}
