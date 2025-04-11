9f986006558844e61561b9b83e60e5a8cf884bdc
tdobre88 <tdobre88@gmail.com>
2025-04-11 15:11:51 +0300

    End of section 5

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -14,4 +14,5 @@ public interface IGenericRepository<T> where T : BaseEntity
     void Remove(T entity);
     Task<bool> SaveAllAsync();
     bool Exists(int id);
+    Task<int> CountAsync(ISpecification<T> spec);
 }
