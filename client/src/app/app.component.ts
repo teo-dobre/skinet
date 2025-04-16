65e4fde20eb6564ed722101242ef018a23e47382
Teodor Dobre <tdobre88@gmail.com>
2025-04-16 15:27:04 +0300

    End of section 11

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { ShopComponent } from './features/shop/shop.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
