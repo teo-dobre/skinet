ff34c8b4606a4e053a12f2a2d53af6df05a8d53b
Teodor Dobre <tdobre88@gmail.com>
2025-04-17 13:24:28 +0300

    End of section 12

---
+++ Core/Entities/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities;
+
+public class CartItem
+{
+    public int ProductId { get; set; }
+    public required string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public required string pictureUrl { get; set; }
+    public required string Brand { get; set; }
+    public required string Type { get; set; }
+}
