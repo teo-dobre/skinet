3b0371d1b006cc4b9bcef1329621db647064011b
Teodor Dobre <tdobre88@gmail.com>
2025-04-25 12:11:09 +0300

    End of section 14

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
+  standalone: true,
   imports: [],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
